1b66f6e188a3d950b7e4f9c11b4114d8ca6e8c2f
glz <100523062+glz99@users.noreply.github.com>
2022-06-13 10:04:27 +0200

    auskommentiert

--- main.py
+++ main.py
@@ -16,14 +16,12 @@ from rechnen.alarm import rechnen3
 from rechnen.co2 import co2_sparen
 
 app = Flask("Daten")
-
-
 app = Flask("__name__")
 
 """Hauptseite"""
 @app.route("/", methods=["get", "post"])
 def index():
-    alarm = rechnen() #diese Funktionen der anderen Datei erhalten hier Namen (alarm, alarm1 usw.)
+    alarm = rechnen()  #diese Funktionen der anderen Datei erhalten hier Namen (alarm, alarm1 usw.)
     alarm1 = rechnen1()
     alarm2 = rechnen2()
     alarm3 = rechnen3()
@@ -33,7 +31,8 @@ def index():
             meine_sammlung = []
             with open('daten2.json', 'w') as open_file:
                 json.dump(meine_sammlung, open_file, indent=3, separators=(',', ':'))
-    #es soll die drei Funktionen returnen, bei welchen die Rechungen zur Entsorgung gemacht wurden
+
+    #es soll die drei Funktionen returnen, bei welchen die Rechnungen zur Entsorgung gemacht wurden
     return render_template("index.html", alarm=alarm, alarm1=alarm1, alarm2=alarm2, alarm3=alarm3)
 
 
@@ -41,69 +40,66 @@ def index():
 @app.route("/form", methods=["get", "post"])
 def form():
 
-    if request.method.lower() == "get": #method get, post (Formularfelder) Daten werden abgeholt und dann weiter
+    if request.method.lower() == "get":   #method get, post (Formularfelder) Daten werden abgeholt und dann weiter
         #verarbeitet
         return render_template("formular.html")
     if request.method.lower() == "post":
 
-
-
-        a=request.form.get("Datum")  # Verknüpfung zu json
-        b=request.form.get("Was")   #Verknüpfung zu json
-        c=request.form.get("Masseinheit") #Verknüpfung zu json
-        d=request.form.get("Anzahl") #Verknüpfung zu json
+        a = request.form.get("Datum")  # Verknüpfung zu json
+        b = request.form.get("Was")   #Verknüpfung zu json
+        c = request.form.get("Masseinheit")   #Verknüpfung zu json
+        d = request.form.get("Anzahl")   #Verknüpfung zu json
         meine_sammlung = {"Datum": a, 'Was': b, "Masseinheit": c,  "Anzahl": d}
-        #request holt Daten aus Formular und tut Sie in den Dictionary "meine_sammlung. Dann speichert es diese Daten
+        #request holt Daten aus Formular und tut Sie in den Dictionary "meine_sammlung". Dann speichert es diese Daten
         #in die json datei
 
         #summe.json
-        data=opendatei1()  # Datei öffnen von der daten1.py
-        data.clear() #daten clearen bevor Daten dazugefügt werden, um immer die aktuellste Anzahl zu haben
+        data = opendatei1()  # Datei öffnen von daten1.py
+        data.clear()   #daten clearen bevor Daten dazugefügt werden, um immer die aktuellste Anzahl zu haben
         summe = form1()  # kommt von alarm.py (Funktion welche summen ausgibt)
         data.append(summe)  # Daten zur json Datei (summe.json) hinzufügen
         storedatei1(data)  # Eingaben von summe.json speichern
 
         #summe.json
-        data=opendatei() #Datei öffnen von der Daten.py
-        data.append(meine_sammlung) #Daten zur json Datei hinzufügen
+        data = opendatei()   #Datei öffnen von der Daten.py
+        data.append(meine_sammlung)   #Daten zur json Datei hinzufügen
         storedatei(data)  #Eingaben von daten2.json speichern
 
+        return render_template("formular.html", name="meine_sammlung")   #meine Sammlung ist daten2.json
 
-        return render_template("formular.html", name="meine_sammlung") #meine Sammlung ist daten2.json
 
+"""Auswertungsseite"""
 
-"""Auswertungseite"""
-
-@app.route ("/auswertung", methods=["POST", "GET"])
+@app.route("/auswertung", methods=["POST", "GET"])
 def auswerten():
-    if request.method.lower() == "get": #get post (Formular) für den Filter button auf der HTML seite
+    if request.method.lower() == "get":   #get post (Formular) für den Filter button auf der HTML seite
         return render_template("auswertung.html")
     if request.method.lower() == "post":
-        Was = request.form.get("Was") #refrenziert mit der unteren if Funktion
-    auswertungs_liste = [] #list erstellen leer für die HTML seite
+        was = request.form.get("Was")   #refrenziert mit der unteren if Funktion
+    auswertungs_liste = []   #list erstellen leer für die HTML seite
     data = opendatei()  #Json Datei öffnen
     for element in data:
-        if element["Was"] == Was: #Element Was ist Pet, Glas oder Karton aus json liste ansprechen
-            auswertungs_liste.append([element["Datum"],element["Was"], element["Masseinheit"], element["Anzahl"]])
+        if element["Was"] == was:   #Element "Was" ist Pet, Glas oder Karton aus json liste ansprechen
+            auswertungs_liste.append([element["Datum"], element["Was"], element["Masseinheit"], element["Anzahl"]])
             # für jedes Element in der jason Datei soll es nun jeweils
-            # die Elemente zu der  auswertungs_liste dazutun
-    return render_template("auswertung.html", liste=auswertungs_liste) #ausgabe des Htmls und der auswertungs_liste
+            # die Elemente zu der auswertungs_liste dazutun
+    return render_template("auswertung.html", liste=auswertungs_liste)   #ausgabe des Htmls und der auswertungs_liste
 
 
 """Seite mit co2"""
-@app.route ("/co2")
+@app.route("/co2")
 def c02():
-    co2 = co2_sparen() #die Funktion der anderen Datei erhält hier einen Namen (co2 usw. und wird returned)
+    co2 = co2_sparen()   #die Funktion der anderen Datei erhält hier einen Namen (co2 usw. und wird returned)
     return render_template("co2.html", co2=co2)
 
-"""Seite mit statistik"""
-@app.route ("/statistik")
 
-def hello_auswertung():
+"""Seite mit statistik"""
+@app.route("/statistik")
+def auswertung():
     div = viz()
 
     try:
-        with open("summe.json") as open_file:
+        with open("summe.json") as open_file:             #summe.json wird geöffnet um nacher zu bearbeiten
             data = json.load(open_file)
     except FileNotFoundError:
         data = {}
@@ -113,35 +109,33 @@ def hello_auswertung():
 
 def get_data():
 
-    sorte = ["Glas", "Pet", "Karton"]
-    anzahl = []
+    sorte = ["Glas", "Pet", "Karton"]    #Glas Pet und Karton als Sorte sind vordefiniert, die bleiben
+    anzahl = []                          #für die Anzahl wird eine neue Liste geöffnet und die Anzahle dort rein gespeichert
     try:
         with open("summe.json") as open_file:
             data = json.load(open_file)
     except FileNotFoundError:
         data = {}
+                                    #Liste in Liste darum zwei mal [][]
+    anzahl.append(data[0][0])       #zu der Anzahlliste wird jeweils der 1. eintrag aus dem ersten eintrag hinzugefügt
+    anzahl.append(data[0][1])       #dann der zweite Eintrag aus dem ersten Eintrag
+    anzahl.append(data[0][2])       #dann der dritte Eintrag aus dem ersten Eintrag
 
-    anzahl.append(data[0][0])
-    anzahl.append(data[0][1])
-    anzahl.append(data[0][2])
-
-
-
-    return sorte, anzahl
+    return sorte, anzahl            #mit dem Return sorte und Anzahl wird dann unten weitergerechnet
 
 
 def viz():
     sorte, anzahl = get_data()
-    fig = px.bar(x=sorte, y=anzahl)
+    fig = px.bar(x=sorte, y=anzahl)    #hier wird definiert, welche Achse von welchen Daten gebraucht wird
     fig.update_layout(
-        xaxis_title="Sorte",
+        xaxis_title="Sorte",   #Achsentitel
         yaxis_title="Anzahl",
     )
 
     div = plot(fig, output_type="div")
 
-    return div
-
+    return div              #der Funktion viz() wird oben ein Name gegeben (div) diese wird dann returnt
+                            #diese Funktion wird dann als jinja auf der Seite ausgegeben
 
 """Wenn der Name der Datei mit Main übereinstimmt, führt es die App auf dem Port 5000 aus"""
 
